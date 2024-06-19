9614156d7405689276862336d1a6fca88a8e7fa4
althaafka <althaafka004@gmail.com>
2024-06-19 16:06:24 +0700

    register page

---
+++ pages/register/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <AuthForm mode="register"/>
+</template>
+  
+
+<script>
+import AuthForm from '~/components/AuthForm.vue';
+
+export default {
+  components: {
+    AuthForm
+  }
+}
+</script>
