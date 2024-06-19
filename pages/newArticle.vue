aa7a6088d6dbc3715c65951e0e53c3c351ba9f54
asyifashfr <asyifashfr.com>
2024-06-19 18:24:47 +0700

    benerin tampilan article page

--- pages/newArticle.vue
+++ pages/newArticle.vue
@@ -25,12 +25,14 @@
               required
             ></textarea>
           </div>
-          <button
-            type="submit"
-            class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-          >
-            Publish
-          </button>
+          <div class="flex justify-end">
+            <button
+                type="submit"
+                class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+            >
+                Publish
+            </button>
+          </div>
         </form>
       </div>
     </div>
