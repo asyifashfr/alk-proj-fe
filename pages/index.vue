c1f043ad7fc5cff1e4da5c3ccba774a38e3c035c
althaafka <althaafka004@gmail.com>
2024-06-19 18:48:50 +0700

    integrate manage article

--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@
     },
     async mounted() {
       try {
-        const response = await fetch('http://localhost:8080/articles');
+        const response = await fetch('http://localhost:8080/articles/all');
         this.articles = await response.json();
         this.mainArticle = this.articles[0];
         console.log(this.articles)
