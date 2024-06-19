4ec80c664e23334ce16e0b8780945b3d7bdfdbb8
althaafka <althaafka004@gmail.com>
2024-06-19 16:06:08 +0700

    feat login page

---
+++ pages/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <AuthForm mode="login"/>
+</template>
+  
+
+<script>
+import AuthForm from '~/components/AuthForm.vue';
+
+export default {
+  components: {
+    AuthForm
+  }
+}
+</script>
