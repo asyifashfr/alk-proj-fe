d8f19c14f1b453d0fe97baf3bed51f7e6efd5417
asyifashfr <asyifashfr.com>
2024-06-21 09:36:08 +0700

    add: dependency

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,13 +1,29 @@
 <template>
   <nav>
       <ul>
-        <li :class="{ 'active': isActive('/') }"><a href="/">Home</a></li>
+        <li :class="{ 'active': isActive('/') }">
+          <a href="/">
+            <i class="fas fa-home text-white"></i>
+          </a>
+        </li>
         <li v-if="isLoggedIn" :class="{ 'active': isActive('/manageArticle') }">
           <a href="/manageArticle">Manage Articles</a>
         </li>
-        <li v-if="isLoggedIn"><a href="#" @click="logout">Logout</a></li>
-        <li v-else><a href="/login">Login</a></li>
+        <li class="ml-4">
+          <a href="/searchPage">
+            <i class="fas fa-search text-white"></i>
+          </a>
+        </li>
+        <li>
+          <button @click="toggleAuthPopup" class="focus:outline-none p-4">
+            <i class="fas fa-user text-white"></i>
+          </button>
+        </li>
       </ul>
+      <div v-if="showAuthPopup" class="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg">
+        <div v-if="isLoggedIn" @click="logout" class="p-4 cursor-pointer hover:bg-gray-200 rounded-lg">Logout</div>
+        <div v-else @click="redirectToLogin" class="p-4 cursor-pointer hover:bg-gray-200 rounded-lg">Login</div>
+      </div>
   </nav>
 </template>
 
@@ -15,9 +31,20 @@
 import { useRoute } from 'vue-router';
 
 export default {
+props: {
+  searchQuery: {
+    type: String,
+    required: true
+  },
+  filterArticles: {
+    type: Function,
+    required: true
+  }
+  },
 data() {
   return {
-    isLoggedIn: true
+    isLoggedIn: true,
+    showAuthPopup: false
   };
 },
 setup() {
@@ -41,6 +68,12 @@ methods: {
     localStorage.removeItem('token');
     this.isLoggedIn = false;
     this.$router.go(); 
+  },
+  redirectToLogin() {
+    this.$router.push('/login');
+  },
+  toggleAuthPopup() {
+    this.showAuthPopup = !this.showAuthPopup;
   }
 }
 }
