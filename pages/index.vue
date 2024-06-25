626c0cebad0433418c26038d3095007114e05237
althaafka <althaafka004@gmail.com>
2024-06-25 09:16:48 +0700

    feat: image

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
       <div class="flex justify-around mt-4 gap-8 mr-10 ml-10 mb-8">
         <div v-for="(article, index) in randomArticles" :key="index" class="w-1/3 p-4 bg-white shadow-lg rounded-lg hover:shadow-2xl" @click="goToArticlePreview(article)">
           <div class="w-full h-40 flex-shrink-0 mr-4 bg-gray-200 mb-8">
-              <NuxtImg :src="article.photo_url" alt="Article Image" class="w-full h-full object-cover rounded-md" />
+              <NuxtImg :src=getImageUrl(article.photo_url) alt="Article Image" class="w-full h-full object-cover rounded-md" />
           </div>
           <h3 class="text-lg font-semibold mb-1">{{ article.title }}</h3>
           <span class="text-gray-600 text-sm block mb-4">by {{ article.username }}</span>
@@ -78,6 +78,9 @@ export default {
       } else {
         this.$router.push('/login');
       }
+    },
+    getImageUrl(photoUrl){
+      return photoUrl ? `http://localhost:8080${photoUrl}` : 'https://via.placeholder.com/150';
     }
   }
 }
